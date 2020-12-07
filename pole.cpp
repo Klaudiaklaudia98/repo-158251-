fd682a17499f624bf1d3436ad0cdf74cf9f70a7f
Klaudiaklaudia98 <75248924+Klaudiaklaudia98@users.noreply.github.com>
2020-12-07 09:42:29 +0100

    Update pole.cpp

--- pole.cpp
+++ pole.cpp
@@ -4,8 +4,8 @@ double wynik, liczba; // double - liczba zmiennoprzecinkowa
 int main()
 {
     using namespace std;
-    cout << "Podaj liczbę: ";
-    cin >> liczba;
+    cout << "Podaj liczbę: ";//! Wyślij liczbę w celu wyświetlenia strumienia znaków\
+    cin >> liczba; //! Akceptacja wejścia ze standardowego urządzenia wejściowego\
     wynik = liczba*liczba;
         cout << "liczba " << liczba << " podniesiona do kwadratu wynosi " << wynik << endl;
     return wynik;
